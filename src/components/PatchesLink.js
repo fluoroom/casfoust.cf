5437f795f5ec103a82981e8555a391c21c75ea51
fluoroom <nico.vial.giudici@gmail.com>
2020-09-17 04:16:20 -0300

    fixed patches link style

--- src/components/PatchesLink.js
+++ src/components/PatchesLink.js
@@ -3,10 +3,13 @@ import { Link } from "gatsby"
 import styled from 'styled-components'
 
 const StyledLink = styled(props => <Link {...props} />)`
-  opacity:0.8;
+  color:rgba(255,255,255,0.8);
+  padding:0.5em 1em;
+  border:1px dotted #ccc;
+  background-color:rgba(0,0,0,0.5);
   &:hover{
-      text-decoration:underline;
-      opacity:1;
+    text-decoration:underline;
+    color:rgba(255,255,255,1);
   }
 `;
 
@@ -17,12 +20,6 @@ const Nav = (noPatches) => {
       <p className="patcheslink"><StyledLink to="/patches/">microKORG Patches!</StyledLink></p>
       <style jsx>
         {`
-        .patcheslink{
-            display:inline-block;
-            padding:0.5em 1em;
-            border:1px dotted #ccc;
-            background-color:rgba(0,0,0,0.5);
-          }
         `}
       </style>
     </>
