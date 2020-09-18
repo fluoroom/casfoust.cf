2df1d8997c6d0fb3d3839154f9d62e858cabfeb3
fluoroom <nico.vial.giudici@gmail.com>
2020-09-18 20:47:17 -0300

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
           // Setting this parameter is optional
           anonymize: true,
           // Setting this parameter is also optional
-          respectDNT: true,
+          respectDNT: false,
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Delays sending pageview hits on route update (in milliseconds)
@@ -40,7 +40,7 @@ module.exports = {
           // Any additional optional fields
           sampleRate: 5,
           siteSpeedSampleRate: 10,
-          cookieDomain: "",
+          cookieDomain: "casfoust.cf",
         },
       }
     },
